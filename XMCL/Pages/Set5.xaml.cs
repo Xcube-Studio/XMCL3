0c570347fd39762ed89c99a4bd02574f76eecc19
zhuxingyan2008 <1903518149@qq.com>
2020-06-13 17:52:43 +0800

    update 反馈 (待xuan修UI)

--- XMCL/Pages/Set5.xaml.cs
+++ XMCL/Pages/Set5.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using MySql.Data.MySqlClient;
 using System;
+using System.IO;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -80,10 +82,98 @@ namespace XMCL.Pages
                 MainWindow.ShowTip("从远程数据库拉取反馈失败,请重试", 1);
             }
         }
+        private void Loaded_()
+        {
+            Frame.Visibility = Visibility.Collapsed;
+            try
+            {
 
+                Latest.Children.Clear();
+
+                Task.Run(() =>
+                {
+                    string strcon = "server=106.14.64.250;User Id=User;password=User20202020server;Database=User";
+                    string sql = $"select * from issues";
+                    MySqlConnection conn = new MySqlConnection(strcon);
+                    conn.Open();
+                    MySqlCommand cmd = new MySqlCommand(sql, conn);
+                    MySqlDataReader reader = cmd.ExecuteReader();
+                    int[] id = new int[200], state = new int[200];
+                    string[] issues = new string[200], by = new string[200], reply = new string[200], replied_by = new string[200], title = new string[200];
+                    int i = 1;
+                    while (true)
+                    {
+                        try
+                        {
+
+                            reader.Read();
+                            id[i] = (int)reader[0];
+                            state[i] = (int)reader[5];
+                            issues[i] = (string)reader[1];
+                            reply[i] = (string)reader[2];
+                            replied_by[i] = (string)reader[3];
+                            by[i] = (string)reader[4];
+                            title[i] = (string)reader[6];
+                            if (i > id[i])
+                                break;
+                            i++;
+                        }
+                        catch { break; }
+                    }
+                    this.Dispatcher.BeginInvoke(new Action(() =>
+                    {
+
+                        for (int f = 1; f < i; f++)
+                        {
+
+                            ListBoxItem listBoxItem = new ListBoxItem();
+                            ListBoxItem listBoxItem1 = new ListBoxItem();
+                            listBoxItem.Margin = listBoxItem1.Margin = new Thickness(20, 0, 20, 0);
+                            listBoxItem.Content = "(XMCL-" + id[f].ToString() + ") " + title[f] + "  By：" + by[f];
+                            listBoxItem.Tag = "XMCL" + id[f].ToString();
+                            Latest.Children.Add(listBoxItem);
+
+                        }
+
+                        pb1.Visibility = Visibility.Collapsed;
+
+
+                    }));
+                });
+            }
+            catch
+            {
+                MainWindow.ShowTip("从远程数据库拉取反馈失败,请重试", 1);
+            }
+        }
         private void Toissues_Click(object sender, RoutedEventArgs e)
         {
+            string str = AppDomain.CurrentDomain.BaseDirectory;
+            Frame.Visibility = Visibility.Visible;
             Frame.Navigate(new SubPage2());
+            FileStream fs = new FileStream(str+"tmp1001.tmp", FileMode.OpenOrCreate, FileAccess.ReadWrite); //可以指定盘符，也可以指定任意文件名，还可以为word等文件
+            StreamWriter sw = new StreamWriter(fs); // 创建写入流
+            sw.WriteLine("tmp"); // 写入Hello World
+            sw.Close();
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    if (!File.Exists(str+"tmp1001.tmp"))
+                    {
+                        break;
+                    }
+                    Thread.Sleep(1000);
+                }
+                Dispatcher.BeginInvoke(new Action(() =>
+                {
+                    Frame.Visibility = Visibility.Collapsed;
+                    Loaded_();
+                }));
+                
+                
+            });
+            
         }
         //保留List的点击事件 SubPage3.xaml
 
--- XMCL/Pages/SubPage2.xaml.cs
+++ XMCL/Pages/SubPage2.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows.Controls;
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Windows.Controls;
+using System.Windows;
+using System.IO;
 
 namespace XMCL.Pages
 {
@@ -10,6 +14,96 @@ namespace XMCL.Pages
         public SubPage2()
         {
             InitializeComponent();
+        }
+        int state;
+        private void ToggleButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            if((bool)state_button.IsChecked)
+            {
+                state_text.Content = "反馈选中：BUG";
+                state = 1;
+            }
+            else
+            {
+                state_text.Content = "反馈选中：建议";
+                state = 10;
+            }
+        }
+
+        private void button1_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            write.Visibility = Visibility.Collapsed;
+            Get.Visibility = Visibility.Visible;
+            System.Threading.Tasks.Task.Run(() =>
+            {
+                string ConString = "server=106.14.64.250;User Id=User;password=User20202020server;Database=User";
+                MySqlConnection conn = new MySqlConnection(ConString);//连接数据库 
+                conn.Open();   //open的时候可以套个try防止boom 
+                int id = 1;
+                int i;
+                string sql = "select * from issues where id='" + id + "'";
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                for (; ; id++)
+                {
+
+                    sql = "select * from issues where id='" + id + "'";
+                    cmd.CommandText = sql;
+                    i = Convert.ToInt32(cmd.ExecuteScalar());
+                    if (i <= 0)
+                    {
+                        MessageBox.Show(id.ToString());
+                        break;
+                    }
+                    
+
+                }
+
+               
+                Dispatcher.Invoke(new Action(() =>
+                {
+                      sql = "Insert Into issues(id, text,reply,replied_by,_by,state,title)Values('" + id + "','" + text.Text + "','null','null','" + Name.Text + "','" + state + "','" + Title.Text + "')";
+                      MessageBox.Show(sql);
+                      cmd.CommandText = sql;
+                }));
+
+             
+                
+                i = Convert.ToInt32(cmd.ExecuteNonQuery());
+
+
+                if (i > 0)
+                    Dispatcher.BeginInvoke(new Action(() =>
+                    {
+                       OK.Visibility = Visibility.Visible;
+                        Get.Visibility = Visibility.Collapsed;
+                    }));
+                
+                else
+                    Dispatcher.BeginInvoke(new Action(() =>
+                    {
+                        MainWindow.ShowTip("提交失败",5);
+                        File.Delete(AppDomain.CurrentDomain.BaseDirectory + "tmp1001.tmp");
+                    }));
+                
+
+
+
+
+
+            });
+        }
+
+        private void Page_Loaded(object sender, System.Windows.RoutedEventArgs e)
+        {
+            OK.Visibility = Visibility.Collapsed;
+            Get.Visibility = Visibility.Collapsed;
+            state_text.Content = "反馈选中：BUG(请选择反馈类型)";
+            state = 1;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            File.Delete(AppDomain.CurrentDomain.BaseDirectory+"tmp1001.tmp");
         }
     }
 }
